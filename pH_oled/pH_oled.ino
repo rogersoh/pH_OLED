2f4bb91ff06e912c765c2832f90756579af43b81
rogersoh <rogersoh@reelmotionhd.com>
2018-04-23 15:19:22 +0800

    change constant name from pHoffset to zeroVolt

--- pH_oled/pH_oled.ino
+++ pH_oled/pH_oled.ino
@@ -1,9 +1,9 @@
 /*
- # This sample code is used to test the pH meter V1.0.
- # Editor : YouYou
- # Ver    : 1.0
- # Product: analog pH meter
- # SKU    : SEN0161
+  # This sample code is used to test the pH meter V1.0.
+  # Editor : YouYou
+  # Ver    : 1.0
+  # Product: analog pH meter
+  # SKU    : SEN0161
 */
 #include <SPI.h>
 #include <Wire.h>
@@ -15,12 +15,13 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 #define SensorPin A0            //pH meter Analog output to Arduino Analog Input 0
 #define pHoffset 2.0587           //deviation compensate
+#define OFFSET 0
 #define LED 13
 #define samplingInterval 20
 #define printInterval 800
 #define ArrayLenth  40    //times of collection
 int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
-int pHArrayIndex=0;    
+int pHArrayIndex = 0;
 void setup(void)
 {
   Serial.begin(9600);
@@ -38,83 +39,83 @@ void setup(void)
   // Clear the buffer.
   display.clearDisplay();
 
-  pinMode(LED,OUTPUT);   
+  pinMode(LED, OUTPUT);
   Serial.println("pH meter experiment!");    //Test the serial monitor
 
 
   // Clear the buffer.
   display.clearDisplay();
-  
+
 }
 void loop(void)
 {
   static unsigned long samplingTime = millis();
   static unsigned long printTime = millis();
-  static float pHValue,voltage;
-  if(millis()-samplingTime > samplingInterval)
+  static float pHValue, voltage;
+  if (millis() - samplingTime > samplingInterval)
   {
-      pHArray[pHArrayIndex++]=analogRead(SensorPin);
-      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
-      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      pHValue = (7/pHoffset)*voltage;
-      samplingTime=millis();
+    pHArray[pHArrayIndex++] = analogRead(SensorPin);
+    if (pHArrayIndex == ArrayLenth)pHArrayIndex = 0;
+    voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
+    pHValue = (7 / pHoffset) * voltage + OFFSET;
+    samplingTime = millis();
   }
-  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  if (millis() - printTime > printInterval)  //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
   {
-  Serial.print("Voltage:");
-        Serial.print(voltage,4);
-        Serial.print("    pH value: ");
-  Serial.println(pHValue,2);
-        digitalWrite(LED,digitalRead(LED)^1);
-        printTime=millis();
+    Serial.print("Voltage:");
+    Serial.print(voltage, 4);
+    Serial.print("    pH value: ");
+    Serial.println(pHValue, 2);
+    digitalWrite(LED, digitalRead(LED) ^ 1);
+    printTime = millis();
     // text display tests
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.print("Volt:");
-  display.println(voltage,3);
-  display.print("pH: ");
-  display.println(pHValue,2);
-  display.display();
-  display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.print("Volt:");
+    display.println(voltage, 3);
+    display.print("pH: ");
+    display.println(pHValue, 2);
+    display.display();
+    display.clearDisplay();
   }
 }
-double avergearray(int* arr, int number){
+double avergearray(int* arr, int number) {
   int i;
-  int max,min;
+  int max, min;
   double avg;
-  long amount=0;
-  if(number<=0){
+  long amount = 0;
+  if (number <= 0) {
     Serial.println("Error number for the array to avraging!/n");
     return 0;
   }
-  if(number<5){   //less than 5, calculated directly statistics
-    for(i=0;i<number;i++){
-      amount+=arr[i];
+  if (number < 5) { //less than 5, calculated directly statistics
+    for (i = 0; i < number; i++) {
+      amount += arr[i];
     }
-    avg = amount/number;
+    avg = amount / number;
     return avg;
-  }else{
-    if(arr[0]<arr[1]){
-      min = arr[0];max=arr[1];
+  } else {
+    if (arr[0] < arr[1]) {
+      min = arr[0]; max = arr[1];
     }
-    else{
-      min=arr[1];max=arr[0];
+    else {
+      min = arr[1]; max = arr[0];
     }
-    for(i=2;i<number;i++){
-      if(arr[i]<min){
-        amount+=min;        //arr<min
-        min=arr[i];
-      }else {
-        if(arr[i]>max){
-          amount+=max;    //arr>max
-          max=arr[i];
-        }else{
-          amount+=arr[i]; //min<=arr<=max
+    for (i = 2; i < number; i++) {
+      if (arr[i] < min) {
+        amount += min;      //arr<min
+        min = arr[i];
+      } else {
+        if (arr[i] > max) {
+          amount += max;  //arr>max
+          max = arr[i];
+        } else {
+          amount += arr[i]; //min<=arr<=max
         }
       }//if
     }//for
-    avg = (double)amount/(number-2);
+    avg = (double)amount / (number - 2);
   }//if
   return avg;
 }
